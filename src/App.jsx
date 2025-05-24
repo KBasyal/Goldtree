7866f62d2e02503e44658fc21eec69f398691567
kbasyal <kamalbasyal987@gmail.com>
2025-05-24 13:00:55 +0545

    complete task submission

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import RoutingConfig from "./config/routing.config";
+import styles from "./styles/app.module.css";
+import './index.css'
+
+
+const App = () => {
+  return <RoutingConfig/> ;
+};
+
+export default App;
